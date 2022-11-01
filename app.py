7b5ff9c3ec8f0a4513019be84ffe78e6455e204b
mubeen shaik <96565287+shaikmubeen@users.noreply.github.com>
2022-11-01 16:25:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app=Flask(__name__)
 def homepage():
         return "<h1>Hello World</h1>"
 if __name__=="__main__":
-        app.run(host="0.0.0.0",port=8080)
+        app.run(host="0.0.0.0",port=8081)
